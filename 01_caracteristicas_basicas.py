901e2f6c5a4d114e55e0b29a19e4c1081a3fa4b0
das <davidvb_6@hotmail.com>
2025-01-29 17:11:47 +0100

    ficheros iniciales

---
+++ 01_caracteristicas_basicas.py
@@ -0,0 +1,65 @@
+"""
+CARACTERISTICAS BÁSICAS DE PYTHON
+
+##Variables
+Una variable es un espacio en memeoria para guardar datos
+por tanto es un contenedor
+
+Para crear una variable...
+identificador = valor
+
+Hay reglas para llamar a los identificadores (nombre variable)
+
+No se puede hacer:
+1var (empezar por numero, pueded contener)
+$var || va$r (contener caracteres especiales)
+
+No DEBEMOS hacer (no son exactamente errores):
+Contener caracteres fuera del ASCII
+Empezar por guion bajo(reservado para determinadas situaciones, por convenio)
+
+Que DEBEMOS hacer
+Nombrar a nuestras variables con palabras descriptivas
+Podemons usar mas de una palabra separadas por un guion bajo (directiva PEP-8)
+
+"""
+git config #para configurar git, SOLO SE HACE UNA VEZ
+--global # para todos los proyectos git, sin especificar estandard
+git config --global user.name "user name" #sino hi ha espais no cal ""
+git config --global user.email "email"
+git config core.autocrlf true   #true para windows | input para mac i linux
+    autocrlf #windows añade un caracter al final, esto hace que git lidie con él automaticamente al subir/descargar
+             #correccion del salto de linea
+git config --global core.editor "code --wait" #para elegir editor
+
+git config --global -e #para ver la configuracion actual abre fichero git.config (e, editar)
+
+git init #inicia un repositorio git vacío
+
+fases de git
+|---------------------------------------------------------------|-------|
+    git                                                         |github
+ |working/unstage   |stage          |commit                     |server |
+ ---------U----------------A--------------------M------------------------------------
+ |donde estan to-   |los ficheros   |mueves ficheros de stage   |subir al servidor
+ |dos los ficheros  |que pasas para |y añades descripcion
+ |                  |control de ver |
+                    |(contraseñas o |
+                    |ficheros muy   |
+                    |pesados no,    |
+                    |config files)  |
+
+git status #da info de como esta en este momento el repositorio
+
+#crear fichero ".gitignore" lista de ficheros que queremos que git ignore (gitignore.io)
+
+git add 
+    .   #todo menos lo del gitignore
+    0*  #todo lo que empieze por 0
+        #o nombres de fichero separados con espacios
+git add 0* code0.py
+
+git rm --cache filename #para quitarlo de la fase stage a Untracked
+
+git commit -m "mensaje" #pasa de fase, a punto para subirlo que ya estan i no se deberian editar
+            #-m message
---
+++ code0.py
@@ -0,0 +1,37 @@
+
+#Imprimeix una piramide
+#############################
+#rang = 5    #altura piramide
+print("Escriu l'altura de la piramide :")
+rangi = input()
+rang = int(rangi)
+
+print("De que vols que estigui feta la piràmide? Escriu un caracter :")
+char = input()
+
+#print("len" , len(char))
+if len(char) == 1 :
+    for x in range(rang):
+        print(char * x)
+    c=x+1
+    for x in range(rang):
+        print(char * c)
+        #print(c)
+        c=c-1
+    else : 
+        print("escriu nomes un caracter : ")
+        char = input()
+
+"""
+from datetime import datetime
+print(datetime.now())
+
+exponencial = 2 ** 3    #(2^3)
+exponencial = 2 ** 0.5  #raiz cuadrada
+division_entera = 20 // 6   #te da solo la parte entera
+modulo = 20 % 3     #el resto de la division (el entero mas cercano 6*3=18 -->18-20= 2) so % 2 ==0 el numero es par
+
+texto_formateado texto = f"{t1} {t2}" #vas vcambiando las variables y el formato a parte
+texto final = "{} {}".format{t1, t2}
+
+"""
