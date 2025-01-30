fb8e777b00c262695a023be1790f6ec9f62585a0
das <davidvb_6@hotmail.com>
2025-01-30 15:30:16 +0100

    upup

--- 04_strings.py
+++ 04_strings.py
@@ -2,18 +2,23 @@
 String - Cadenas de texto
 """
 
-Str1 = 'hola1'
-Str2 = "hola2"
-Str3 = """hola3"""
+#Str1 = 'hola1'
+#Str2 = "hola2"
+#Str3 = """hola3"""
 
-print(Str1 + " " + Str2 + " " + Str3)
+#print(Str1 + " " + Str2 + " " + Str3)
 
 print("nombre?")
 nombre = input()
+#print(type(nombre))
 print("apellido?")
 apellido = input()
+#print(type(apellido))
 print("edad?")
 edad = input()
+edadint = int(edad)
+#print(type(edadint))
 datos="Me llamo " + nombre + " " + apellido + "y tengo " + edad + " años"
 
 print(datos)
+
