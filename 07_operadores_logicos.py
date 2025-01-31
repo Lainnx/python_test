ef2674fb648029a65cc5049af14a27a9e02be82b
das <titicaca@gmail.com>
2025-01-31 16:32:13 +0100

    excepciones

---
+++ 07_operadores_logicos.py
@@ -0,0 +1,17 @@
+#OPERADORES LOGICOS
+#Sirven para poder combinar diversas condiciones
+"""
+#AND    || x and y || SOLO SI x e y son True    (js &&)
+print("True and True", True and True)       #True
+print("True and False", True and False)     #False
+print("False and False", False and False)   #False
+
+#OR     || x or y  || si x O y son True         (js ||)
+print("True or True", True or True)     #True
+print("True or False", True or False)   #True
+print("False or False", False or False) #False
+
+#NOT    || Negacion                             (js !)
+print("not True", not True)     #False
+print("not False", not False)   #True
+"""
---
+++ 08_excepciones.py
@@ -0,0 +1,29 @@
+#EXCEPCIONES
+#Son errores que se producen durante la ejecucion del programa y lo interrumpen
+
+import os
+os.system("cls")        #para limpiar pantalla en windows
+
+
+
+"""
+
+try:            #lo que intenta hacer
+    print(1/0)                                 #operacion imposible para python
+    print("despues de la division por 0")   #no se ejecuta, cuando encuentra el fallo salta al except
+except:         #lo que se va a ejecutar cuando se produce el error
+    print("nose puede dividir por 0")   #en vez de parar el programa se ejecuta la excepcion y continua ejecutando el programa
+
+print("el prgrama continua... ")
+"""                                     #capturamos posibles errores criticos y los gestionamos para que tengan una salida digna
+#try /except -- si hay try debe haber except,  el try dice al programa que esté atento a posibles errores
+try:
+    num = float(input("un numero? "))
+except ValueError:                              #error de conversion de tipos
+    print("has de introducir un numero en cifras")
+except ZeroDivisionError:                       #error por division por 0
+    print("no se puede dividir por 0")
+except:                                         #error genérico
+    print("ha ocurrido un error")
+
+print("el programa continua")
