9c36fc94412684e58f78f6ad3cd0788772336361
das <tree@gmail.com>
2025-02-12 16:33:03 +0100

    ej dicc+

--- ejercicio_diccionarios_.py
+++ ejercicio_diccionarios_.py
@@ -13,7 +13,10 @@ Necesitamos un menú con estas opciones:
 X. Salir
 """
 import os
-
+def print_users(lista_usuarios):
+    print("Lista de usuarios: ")
+    for name in lista_usuarios:
+        print(name["nombre"])
     
 lista_usuarios = []
 lista_exist = []            #NO DECLARAR VARIABLES DENTRO DE BUCLES QUE SE REINICIAN CADA ITERACION!
@@ -35,21 +38,21 @@ while True:
             # existe = 0
             new_user = input("Introduce el nombre del usuario que deseas añadir: ").strip().title()     #title pone la primera letra mayus y el resto en minus
             # usuario_nuevo = {"nombre":user,"visitas":0}
-            if lista_usuarios:                                      #si no hay nada en la lista el for no tiene sobre qué iterar
+            # if lista_usuarios:                                      #si no hay nada en la lista el for no tiene sobre qué iterar
                 # for user in lista_usuarios:                         #user es un diccionario, iteramos sobre todos los de la LISTA DE USUARIOS
                 #     if user["nombre"] not in lista_exist:           
                 #         lista_exist.append(user["nombre"])          
                 #         print("EXIST: ",lista_exist)
                 #         print("user", user["nombre"])
 
-                if new_user in lista_exist:                             #si new_user esta el la lista exist(en lista_usuarios hay un nombre igual)-> este usuario ya existe
-                    print("Este usuario ya existe: ", new_user)
+            if new_user in lista_exist:                             #si new_user esta el la lista exist(en lista_usuarios hay un nombre igual)-> este usuario ya existe
+                print("Este usuario ya existe: ", new_user)
                     
-                else:
-                    usuario_nuevo = {"nombre":new_user,"visitas":0}     #si new_user no esta en lista exist (||) se crea un nuevo usuario y se añade a lista_usuarios
-                    lista_usuarios.append(usuario_nuevo) 
-                    print(f"Usuario {new_user} añadido correctamente")
-                    lista_exist.append(usuario_nuevo["nombre"])         #se añaden los nombres que no estan ya, en la lista, si estamos añadiendo un usuario nuevo no estará en 
+            else:
+                usuario_nuevo = {"nombre":new_user,"visitas":0}     #si new_user no esta en lista exist (||) se crea un nuevo usuario y se añade a lista_usuarios
+                lista_usuarios.append(usuario_nuevo) 
+                print(f"Usuario {new_user} añadido correctamente")
+                lista_exist.append(usuario_nuevo["nombre"])         #se añaden los nombres que no estan ya, en la lista, si estamos añadiendo un usuario nuevo no estará en 
                     # print(lista_usuarios, lista_exist)                #lista exist
                 
                     # if user["nombre"] == new_user:
@@ -63,13 +66,14 @@ while True:
                 #     print("Ese usuario ya existe")
                 #     print("user: ",new_user)
                     # user["visitas"] += 1
-            else:                                                       #el primer elemento de la lista,
-                usuario_nuevo = {"nombre":new_user,"visitas":0}
-                lista_usuarios.append(usuario_nuevo)
-                print(f"Usuario {new_user} añadido correctamente")
-                lista_exist.append(usuario_nuevo["nombre"])             #para añadir el primer nombre a la lista, sino la lista va con 1 de retraso
+            # else:                                                       #el primer elemento de la lista,
+            #     usuario_nuevo = {"nombre":new_user,"visitas":0}
+            #     lista_usuarios.append(usuario_nuevo)
+            #     print(f"Usuario {new_user} añadido correctamente")
+            #     lista_exist.append(usuario_nuevo["nombre"])             #para añadir el primer nombre a la lista, sino la lista va con 1 de retraso
 
         case "2":
+            print_users(lista_usuarios)
             nueva_visita = input("A que usuario quieres añadir una visita? ").strip().title()
             # print(lista_exist)
             if nueva_visita in lista_exist:
@@ -82,9 +86,7 @@ while True:
                 
 
         case "3":
-            print("Lista de usuarios: ")
-            for name in lista_usuarios:
-                print(name["nombre"])
+            print_users(lista_usuarios)
             visita = input("De que usuario quieres ver las visitas? ").strip().title()
 
             if visita in lista_exist:
